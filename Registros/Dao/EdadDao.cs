cc1783e031616e9535a42351b1f74e7f34be073f
Ramcesch <uramcessch@uamv.edu.ni>
2025-10-01 10:20:01 -0600

    Arreglo edades

---
+++ Registros/Dao/EdadDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registros.Dao
+{
+
+    public class EdadDao
+    {
+        public static int[] edades = new int[10];
+        public static int pos = 0;
+    }
+}
---
+++ Registros/Formulario/FormArreglo.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Registros.Formulario
+{
+    partial class FormArreglo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbEdades = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.lbEdades = new System.Windows.Forms.ListBox();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.gbEdades.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbEdades
+            // 
+            this.gbEdades.Controls.Add(this.lblPromedio);
+            this.gbEdades.Controls.Add(this.lbEdades);
+            this.gbEdades.Controls.Add(this.tbEdad);
+            this.gbEdades.Controls.Add(this.label1);
+            this.gbEdades.Location = new System.Drawing.Point(12, 12);
+            this.gbEdades.Name = "gbEdades";
+            this.gbEdades.Size = new System.Drawing.Size(159, 426);
+            this.gbEdades.TabIndex = 0;
+            this.gbEdades.TabStop = false;
+            this.gbEdades.Text = "Edades";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edad";
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(53, 25);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(100, 22);
+            this.tbEdad.TabIndex = 1;
+            this.tbEdad.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbEdades
+            // 
+            this.lbEdades.FormattingEnabled = true;
+            this.lbEdades.ItemHeight = 16;
+            this.lbEdades.Location = new System.Drawing.Point(10, 61);
+            this.lbEdades.Name = "lbEdades";
+            this.lbEdades.Size = new System.Drawing.Size(120, 276);
+            this.lbEdades.TabIndex = 2;
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.Location = new System.Drawing.Point(7, 344);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(100, 23);
+            this.lblPromedio.TabIndex = 3;
+            this.lblPromedio.Text = "Promedio:0";
+            this.lblPromedio.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // FormArreglo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbEdades);
+            this.Name = "FormArreglo";
+            this.Text = "Arreglo";
+            this.Load += new System.EventHandler(this.FormArreglo_Load);
+            this.gbEdades.ResumeLayout(false);
+            this.gbEdades.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbEdades;
+        private System.Windows.Forms.Label lblPromedio;
+        private System.Windows.Forms.ListBox lbEdades;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.Label label1;
+    }
+}
